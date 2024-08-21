69d39b14231054ac8111f03d60d95837ea5c6452
osvaldocastroo <osvaldocastro2704@gmail.com>
2024-08-21 19:26:02 -0300

    Sitio web creado

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.getElementById('saludarBtn').onclick = function() {
+    alert('Hola!');
+};
+document.getElementById('contenedor').addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
